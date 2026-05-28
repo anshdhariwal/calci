457962a6d1b52ec0708e508299741088447f5568
jigyasaphogat <phogatjigyasa28@gmail.com>
2026-05-28 23:36:17 +0530

    make footer full bleed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Watermark from './components/common/Watermark.jsx';
 function App() {
   return (
     <Router>
-      <main style={{ minHeight: '80vh', padding: '0.5rem 1rem' }}>
+      <main style={{ minHeight: '80vh' }}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
