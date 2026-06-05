ceb173a134eb896475d03053ccffcd50da7fa7df
jigyasaphogat <phogatjigyasa28@gmail.com>
2026-06-05 13:15:22 +0530

    fix public base url warning in development

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/calci/',
+  base: command === 'build' ? '/calci/' : '/',
   server: {
     port: 6969,
   },
-})
+}))
