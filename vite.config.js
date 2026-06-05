9f46be510ee218c9d9aed56a90390762eec9f337
jigyasaphogat <phogatjigyasa28@gmail.com>
2026-06-05 17:31:40 +0530

    update project build base path and eslint configurations

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/calci/' : '/',
+  base: '/calci/',
   server: {
     port: 6969,
   },
-}))
+})
